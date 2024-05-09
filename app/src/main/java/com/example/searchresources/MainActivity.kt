be5c65f448a9e09e30173a42a16edb5a630ee2ef
rlaxodud214 <rlaxodud214@naver.com>
2024-05-09 18:03:02 +0900

    initical project

---
+++ app/src/main/java/com/example/searchresources/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.searchresources
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.searchresources.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+    }
+}
