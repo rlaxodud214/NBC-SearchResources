c380760b54e8ec654c7b40b9eec1a7bd4c0950fd
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-05-09 19:14:27 +0900

    feat: TabLayout, ViewPager2, Fragment 연동 (#1)
    * feat: TabLayout, ViewPager2 UI 연동
    
    refactor: 프로젝트 구조 변경

--- app/src/main/java/com/example/searchresources/ui/searchList/SearchListFragment.kt
+++ app/src/main/java/com/example/searchresources/ui/searchList/SearchListFragment.kt
@@ -1,22 +1,21 @@
-package com.example.searchresources
+package com.example.searchresources.ui.searchList
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.searchresources.databinding.FragmentFirstBinding
+import androidx.fragment.app.Fragment
+import com.example.searchresources.databinding.FragmentSearchListBinding
 
-class FirstFragment : Fragment() {
-    private var _binding: FragmentFirstBinding? = null
+class SearchListFragment : Fragment() {
+    private var _binding: FragmentSearchListBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentSearchListBinding.inflate(inflater, container, false)
 
         return binding.root
     }
